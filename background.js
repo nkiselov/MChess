a064c501e86ee3b6b78aebd8195c02c500b15022
nkiselov <58318608+nkiselov@users.noreply.github.com>
2022-03-07 03:15:19 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,8 @@
+// const chess = new Chess()
+// console.log(chess.pgn())
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('script.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
